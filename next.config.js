ef53c7387a9f29baf7b1bac06c4781ea221965bc
Marianne Gomes <mariannegomes02@gmail.com>
2023-04-25 14:04:52 -0300

    feat: inserting adjustments in poc/react-query

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname);
-    return config;
-  },
-};
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	webpack(config, options) {
+		config.resolve.alias['@'] = path.join(__dirname)
+		return config
+	},
+}
---
+++ src/scripts/createBackstopScenarios.ts
@@ -0,0 +1,44 @@
+import fs from 'fs'
+
+import backstopCurrentJSON from '../backstop.json'
+
+const createBackstopScenarios = (linksArray = []) => {
+	const backStopScenarios = []
+
+	linksArray.forEach((component) => {
+		component.links.forEach((story) => {
+			const componentName = component.name.toUpperCase()
+			const storyName = story.name.toUpperCase()
+
+			backStopScenarios.push({
+				label: `${componentName} ${storyName}`,
+				cookiePath: 'backstop_data/engine_scripts/cookies.json',
+				url: story.url,
+				referenceUrl: '',
+				readyEvent: '',
+				readySelector: '#component-container',
+				delay: 0,
+				hideSelectors: [],
+				removeSelectors: [],
+				hoverSelector: '',
+				clickSelector: '',
+				postInteractionWait: 2000,
+				selectors: ['#component-container'],
+				selectorExpansion: true,
+				expect: 0,
+				misMatchThreshold: 0.1,
+				requireSameDimensions: true,
+			})
+		})
+	})
+
+	backstopCurrentJSON.scenarios = backStopScenarios
+	fs.writeFileSync(
+		'../backstop.json',
+		JSON.stringify(backstopCurrentJSON),
+		null,
+		2
+	)
+}
+
+export default createBackstopScenarios
